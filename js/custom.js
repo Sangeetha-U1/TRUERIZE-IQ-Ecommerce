7fa432bbd92a276aeffbd6071a6699ac7f189e2d
Sangeetha <sangeethauppula2@gmail.com>
2026-05-28 16:14:54 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,15 @@
 // =====================
+// LOGIN CHECK (GATE)
+// =====================
+if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "login.html";
+}
+function logout(){
+    localStorage.removeItem("loggedIn");
+    window.location.href = "login.html";
+}
+// =====================
+
 // YEAR
 // =====================
 function getYear() {
