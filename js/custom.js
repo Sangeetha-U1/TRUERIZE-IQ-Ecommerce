d58a05680999a1af1aed8ea5611d537ac405d22c
Sangeetha <sangeethauppula2@gmail.com>
2026-05-28 17:37:23 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -294,3 +294,4 @@ updateCartCount();
     document.getElementById("minutes").textContent = String(minutes).padStart(2, "0");
     document.getElementById("seconds").textContent = String(seconds).padStart(2, "0");
   }, 1000);
+  
